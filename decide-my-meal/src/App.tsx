ae07c4f39dd68441709d3b158efa3f28d10c6b54
Daniel L <danielkcode@gmail.com>
2022-02-06 17:07:27 -0600

    added navbar component setup

--- decide-my-meal/src/App.tsx
+++ decide-my-meal/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import Navbar from "./components/layout/navbar/Navbar";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div>hello world</div>
     </div>
   );
