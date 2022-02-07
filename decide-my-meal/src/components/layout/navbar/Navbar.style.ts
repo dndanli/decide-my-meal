a2c054de8cf1b4b137345059547abcd4bf018666
Daniel L <danielkcode@gmail.com>
2022-02-07 00:00:44 -0600

    added nav styling

--- decide-my-meal/src/components/layout/navbar/Navbar.style.ts
+++ decide-my-meal/src/components/layout/navbar/Navbar.style.ts
@@ -2,7 +2,18 @@ import styled from "styled-components";
 import Navbar from "./Navbar";
 
 const StyledNavbar = styled(Navbar)`
-  background-color: red;
+  font-size: 1.6rem;
+  .nav-ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .nav-ul li {
+    list-style-type: none;
+    margin-right: 1.8rem;
+  }
+  /* main nav color */
+  background-color: rgb(255, 254, 252);
 `;
 
 export default StyledNavbar;
