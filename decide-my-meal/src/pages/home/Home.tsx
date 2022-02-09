2d4834e51853443d4a07e4a902df781da1018a9e
Daniel L <danielkcode@gmail.com>
2022-02-09 15:50:44 -0600

    removed comments

--- decide-my-meal/src/pages/home/Home.tsx
+++ decide-my-meal/src/pages/home/Home.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import StyledNavbar from "../../components/layout/navbar/Navbar.style";
 import MainForm from "../../components/forms/MainForm";
-/**
- *
- * if only one food input then display the + and - buttons
- * if more than 1 then
- *  then display normal food input
- *  make last food input display + and - buttons
- */
+
 const Home: React.FC = () => {
   return (
     <div>
