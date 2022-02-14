1a6706da635fd0fa5620463f31b8d5f27439d18d
Daniel L <danielkcode@gmail.com>
2022-02-14 14:28:54 -0600

    update

--- decide-my-meal/src/components/layout/result/Result.tsx
+++ decide-my-meal/src/components/layout/result/Result.tsx
@@ -17,8 +17,13 @@ const Result = ({ className, foodChosen }: ResultProp) => {
       <div className="close-btn" onClick={handleOnClose}>
         <AiOutlineCloseCircle />
       </div>
-      <div className="get-restaurants-nearby">
-        <h3>Get restaurants nearby</h3>
+      <div
+        className="get-restaurants-nearby"
+        onClick={() => {
+          console.log("fetching for restaurants nearby");
+        }}
+      >
+        <h3>Find restaurants.</h3>
       </div>
     </div>
   );
