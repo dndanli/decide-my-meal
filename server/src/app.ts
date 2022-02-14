b0bf0a6cbefb810951bd3fd2d8d35c87c28eebf8
Daniel L <danielkcode@gmail.com>
2022-02-14 16:20:43 -0600

    installed nodemon, added sample express server to index.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,15 @@
+import express, { Application, Request, Response } from "express";
 require("dotenv").config();
+
 const API_KEY = process.env.API_KEY;
-console.log(API_KEY);
+
+const app: Application = express();
+const PORT = 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
