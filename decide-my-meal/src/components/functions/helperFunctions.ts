c2af95d0a6db65ef038fbbad9aa468f90d21a31a
Daniel L <danielkcode@gmail.com>
2022-02-17 16:11:02 -0600

    added handleStyling and handleFoodChosen helper functions

--- decide-my-meal/src/components/functions/helperFunctions.ts
+++ decide-my-meal/src/components/functions/helperFunctions.ts
@@ -1,9 +1,11 @@
+import postFormData from "./postFormData";
+import getApiData from "./getApiData";
+
 export const isInputQuantityAccepted = (length: number): boolean => {
   const minLen = 1;
   if (length <= minLen) return false;
   return true;
 };
-
 export const changeErrorStyling = (isVisible: boolean): void => {
   let errorHeader4 = document.querySelector("#food-error") as HTMLElement;
   if (isVisible === true) {
@@ -17,11 +19,28 @@ export const pickRandomFood = (textInputArr: string[]): string => {
   return textInputArr[Math.floor(Math.random() * textInputArr.length)];
 };
 
-export const changeAreaOfComponentVisibility = (isVisible: boolean): void => {
+const changeAreaOfComponentVisibility = (isVisible: boolean): void => {
   let componentArea = document.querySelector(".result-display") as HTMLElement;
   if (isVisible === true) {
     componentArea.style.visibility = "visible";
   } else {
     componentArea.style.visibility = "hidden";
   }
+};
+
+export const handleStyling = (
+  toggleErrorStyle: boolean,
+  toggleArea: boolean
+): void => {
+  changeAreaOfComponentVisibility(toggleArea);
+  changeErrorStyling(toggleErrorStyle);
+};
+
+export const handleFoodChosen = (
+  formData: Object,
+  textInputs: string[]
+): string => {
+  const foodChosen = pickRandomFood(textInputs);
+  postFormData(formData, foodChosen);
+  return foodChosen;
 };
