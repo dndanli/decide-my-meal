cfedccf746aec11bb3eb6d6f92ef504c7e7230d1
Daniel L <danielkcode@gmail.com>
2022-02-16 18:29:16 -0600

    changed routing

--- decide-my-meal/src/components/functions/getApiData.ts
+++ decide-my-meal/src/components/functions/getApiData.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 async function getApiData() {
   try {
-    const respose = await axios.get("http://localhost:5000/get_data");
-    console.log("the response data");
+    const respose = await axios.get("http://localhost:5000/data");
     return respose.data;
   } catch (error) {
     console.log(error);
---
+++ decide-my-meal/src/components/functions/postFormData.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+async function postFormData(formData: Object, foodChosen: string) {
+  console.log("in the post form");
+  console.log(formData,foodChosen);
+  try {
+    await axios.post("http://localhost:5000/data", {
+      formData,
+      foodChosen,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default postFormData;
