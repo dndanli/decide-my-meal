f8d0b7903dbb505ab0b76701b1282a2de8c3ccf9
Daniel L <danielkcode@gmail.com>
2022-02-17 01:35:13 -0600

    added helper functions

---
+++ decide-my-meal/src/components/functions/helperFunctions.ts
@@ -0,0 +1,27 @@
+export const valiDateInputLength = (length: number): boolean => {
+  const minLen = 1;
+  if (length <= minLen) return true;
+  return false;
+};
+
+export const changeErrorStyling = (isVisible: boolean): void => {
+  let errorHeader4 = document.querySelector("#food-error") as HTMLElement;
+  if (isVisible === true) {
+    errorHeader4.style.visibility = "visible";
+  } else {
+    errorHeader4.style.visibility = "hidden";
+  }
+};
+
+export const pickRandomFood = (textInputArr: string[]): string => {
+  return textInputArr[Math.floor(Math.random() * textInputArr.length)];
+};
+
+export const changeAreaOfComponentVisibility = (isVisible: boolean): void => {
+  let componentArea = document.querySelector(".result-display") as HTMLElement;
+  if (isVisible === true) {
+    componentArea.style.visibility = "visible";
+  } else {
+    componentArea.style.visibility = "hidden";
+  }
+};
