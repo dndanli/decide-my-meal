2d04bc054c2cdff48edd07a42ec6d8386e002406
Daniel L <danielkcode@gmail.com>
2022-02-17 21:20:22 -0600

    style changes and website link added

--- decide-my-meal/src/components/layout/list/List.style.ts
+++ decide-my-meal/src/components/layout/list/List.style.ts
@@ -6,19 +6,32 @@ const StyledList = styled(List)`
   background-color: rgba(255, 254, 252, 0.2);
   backdrop-filter: blur(7px);
   color: rgb(255, 254, 252);
-  width: 450px;
-  height: 120px;
+  width: 520px;
+  height: 145px;
   padding: 0.5rem;
   margin: 0 1rem 1rem 1rem;
   border-radius: 8px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-
+  .title {
+    font-size: 1.7rem;
+  }
   .title,
   .phone-number,
-  .address {
+  .address,
+  .website {
     margin-bottom: 0.5rem;
+  }
+
+  .phone-number,
+  .address,
+  .website {
+    font-size: 1.2rem;
+  }
+  .website-link {
+    text-decoration: none;
+    color: #ff9761;
   }
 `;
 
--- decide-my-meal/src/components/layout/list/List.tsx
+++ decide-my-meal/src/components/layout/list/List.tsx
@@ -3,13 +3,30 @@ type ListProps = {
   title: string;
   phoneNumber: string;
   address: string;
+  website: string;
 };
 
-const List = ({ className, title, phoneNumber, address }: ListProps) => {
+const List = ({
+  className,
+  title,
+  phoneNumber,
+  address,
+  website,
+}: ListProps) => {
   return (
     <div className={className}>
       <h2 className="title">{title}</h2>
       <h4 className="phone-number">{phoneNumber}</h4>
+      <h4 className="website">
+        <a
+          className="website-link"
+          href={`//${website}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {website}
+        </a>
+      </h4>
       <h4 className="address">{address}</h4>
     </div>
   );
