4adc32cc27b2251da456d31ab7c7c960b7ffd8c8
Daniel L <danielkcode@gmail.com>
2022-02-20 16:57:35 -0600

    changed port route

--- decide-my-meal/src/components/functions/postFormData.ts
+++ decide-my-meal/src/components/functions/postFormData.ts
@@ -4,7 +4,7 @@ async function postFormData(formData: Object, foodChosen: string) {
   console.log("in the post form");
   console.log(formData, foodChosen);
   try {
-    await axios.post("http://localhost:5000/data", {
+    await axios.post("http://localhost:8080/data", {
       formData,
       foodChosen,
     });
