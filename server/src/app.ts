dfb69f64f2d18fc214630e79da6198ae0ef3aa1b
Daniel L <danielkcode@gmail.com>
2022-02-20 15:27:42 -0600

    added .env port to server-side app.ts file

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ require("dotenv").config();
 const API_KEY = process.env.API_KEY;
 
 const app: Application = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
