322357c6d634006ae8a02476d336642f940537f8
Daniel L <danielkcode@gmail.com>
2022-02-20 20:01:42 -0600

    Revert "changed port route"
    This reverts commit 4adc32cc27b2251da456d31ab7c7c960b7ffd8c8.

--- decide-my-meal/src/components/functions/getApiData.ts
+++ decide-my-meal/src/components/functions/getApiData.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function getApiData() {
   try {
-    const respose = await axios.get("http://localhost:8080/data");
+    const respose = await axios.get("http://localhost:5000/data");
     return respose.data;
   } catch (error) {
     console.log(error);
